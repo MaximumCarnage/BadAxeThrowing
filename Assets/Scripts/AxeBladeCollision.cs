bb018ab44389cc73574b345034168cccd51d89f9
MaximumCarnage <dylansbruton@gmail.com>
2018-06-11 20:45:09 -0400

    working on collision tweaks and axe sticks in target

---
+++ Assets/Scripts/AxeBladeCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeBladeCollision : MonoBehaviour {
+
+	public Rigidbody axeRb;
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "target"){
+			axeRb.constraints = RigidbodyConstraints.FreezeAll;
+		}
+	}
+}
--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -7,39 +7,60 @@ public class TouchInput : MonoBehaviour {
 
 public Rigidbody axeRb;
 public GameObject axeObj;
+public GameObject axeContainer;
 public Vector3 axeVel;
 
+
 public Slider powerSlider;
-private float speed = 0.1f;
-private Vector3 firePos;
-private float rotate = -0.3f;
+private float speed = -0.02f;
+private Vector3 firePos,rotateAngle;
+
+private Quaternion fireRot;
+private float rotate = -0.3f,zIncrem = 2f,yIncrem = 0.4f;
 public bool axeHeld = true;
+
 
 private int timer;
 	// Use this for initialization
 	void Start () {
 		firePos = axeObj.transform.position;
+		fireRot = axeObj.transform.rotation;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		powerSlider.value = axeVel.z/1000;
+		
+		if(axeHeld){
+			axeObj.transform.position =new Vector3(axeObj.transform.position.x+speed,axeObj.transform.position.y,axeObj.transform.position.z);
+		}
+		
+		if(axeObj.transform.position.x<-3 )
+		{
+			speed*=-1;
+		}else if(axeObj.transform.position.x>3){
+			speed*=-1;
+		}
 		Debug.Log(timer);
 		if(Input.GetMouseButton(0) == true && axeHeld){
 			if(axeVel.z<1000){
-				axeObj.GetComponent<Transform>().Rotate ( new Vector3( rotate,0,0) );
+				axeObj.transform.Rotate(0,0,rotate);
 				axeVel.z += 2f;
+				axeVel.y += 0.4f;
 			}
+			
 			
 		}
 		else if(Input.GetMouseButtonUp(0) == true && axeHeld == true){
 			
 			
-			axeObj.transform.eulerAngles = Vector3.zero;
+			axeObj.transform.rotation = fireRot;
 			axeRb.constraints = RigidbodyConstraints.None;
 			axeRb.GetComponent<Rigidbody>().AddForce(axeVel);
 			axeHeld = false;
 			axeVel.z = 0;
+			axeVel.y = 0;
 		}
 		if(!axeHeld){
 			timer++;
@@ -63,7 +84,7 @@ private int timer;
 		timer = 0;
 		axeHeld = true;
 		axeObj.transform.position = firePos;
-		axeObj.transform.rotation = Quaternion.identity;
+		axeObj.transform.rotation = fireRot;
 		axeRb.constraints = RigidbodyConstraints.FreezeAll;
 		axeRb.velocity = Vector3.zero;
 	}
