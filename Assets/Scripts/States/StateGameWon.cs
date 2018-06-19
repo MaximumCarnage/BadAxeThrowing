4d5c44ca56b26490a149552c838504c58b5bc678
Brent Page <Brentpage75@gmail.com>
2018-06-18 21:06:42 -0400

    FSM added
    got fsm working in my scene with the working gameplay from Dylan

---
+++ Assets/Scripts/States/StateGameWon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class StateGameWon : GameState {
+	
+	private float m_countDown = 5f;
+
+	public StateGameWon(GameManager gm):base(gm) { }
+
+	public override void Enter() {
+		
+	}
+
+	public override void Execute() {
+	}
+
+	public override void Exit() {}
+
+		public void PlayGame() {
+		m_gm.NewGameState(m_gm.m_stateGamePlay);
+		
+	}
+	void Update()
+	{
+		
+	}
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
+
+
